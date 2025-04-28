d002fcd1b09c216841985005516c68a4d58a2db2
Mohammed Siddique <khanshahid9066@gmail.com>
2025-04-28 22:37:23 +0530

    My_Portfolio
    active button

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const  allSections = document.querySelector('main-content');
 function PageTransition(){
     // Button click active class
     for(let i = 0; i < secBtn.length; i++){
-        secBtn[i].addEventListner
+        secBtn[i].addEventListner('click', ()=> {
+            let currentBtn = document.querySelector('.active-btn');
+            currentBtn[0].classList = currentBtn[0].className.replace('active-btn', '');
+        } )
     }
 }
