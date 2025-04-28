7d4805d8b3c4e3eca60a0d53a28db2c0a2e6e76c
Mohammed Siddique <khanshahid9066@gmail.com>
2025-04-28 22:28:05 +0530

    My_Portfolio
    active button

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 const sections = document.querySelector('section');
 const secBtns = document.querySelector('controlls');
 const secBtn = document.querySelector('control');
+const  allSections = document.querySelector('main-content');
