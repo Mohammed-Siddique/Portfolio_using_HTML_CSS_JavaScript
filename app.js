fc907e1d46c65dc71aecb963884d8407e4f06876
Mohammed Siddique <khanshahid9066@gmail.com>
2025-05-12 20:33:28 +0530

    My_Portfolio
    active button java script

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const sections = document.querySelector('section');
-const secBtns = document.querySelector('controlls');
-const secBtn = document.querySelector('control');
-const  allSections = document.querySelector('main-content');
+const sections = document.querySelectorAll('section');
+const secBtns = document.querySelectorAll('controlls');
+const secBtn = document.querySelectorAll('control');
+const allSections = document.querySelector('main-content');
 
 function PageTransition(){
     // Button click active class
     for(let i = 0; i < secBtn.length; i++){
-        secBtn[i].addEventListner('click', ()=> {
-            let currentBtn = document.querySelector('.active-btn');
-            currentBtn[0].classList = currentBtn[0].className.replace('active-btn', '');
-            this.className += 'active-btn'
+        secBtn[i].addEventListner('click', function() {
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += 'active-btn';
         } )
     }
 }
