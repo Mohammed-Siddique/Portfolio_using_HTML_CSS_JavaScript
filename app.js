063a9e569df4cbea718a220e5b9c573cb0fab698
Mohammed Siddique <khanshahid9066@gmail.com>
2025-06-08 22:13:14 +0530

    Portfolio_MERN
    DOM manipulation using javascript

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const sections = document.querySelectorAll('section');
-const secBtns = document.querySelectorAll('controls');
-const secBtn = document.querySelectorAll('control');
-const allSections = document.querySelector('main-content');
+const sections = document.querySelectorAll('.section');
+const secBtns = document.querySelectorAll('.controls');
+const secBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
 
 function PageTransition(){
-    // Button click active class
     for(let i = 0; i < secBtn.length; i++){
-        secBtn[i].addEventListner('click', function() {
+        secBtn[i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.active-btn');
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
             this.className += 'active-btn';
-        } )
+        })
     }
 
     // Section active class
